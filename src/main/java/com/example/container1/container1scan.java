01fd160960c00dabf195fd301622a8bb8849031d
rvaghani <rt691750@dal.ca>
2024-07-12 00:45:21 -0300

    test1

--- src/main/java/com/example/container1/container1scan.java
+++ src/main/java/com/example/container1/container1scan.java
@@ -104,6 +104,7 @@ public class container1scan {
                 System.out.println("the line "+ line);
                 String[] parts = line.split(",");
                 System.out.println("the line parts: " + parts);
+                //test2
                 System.out.println("the length of the line parts: " + parts.length);
 
                 if (parts.length != 2) {
